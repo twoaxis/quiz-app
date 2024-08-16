23e5b9a9313cd4ea144763ca500808c6851a36d2
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-08-17 00:44:22 +0300

    finish the ui of the sign up

--- mobile/lib/pages/signup.dart
+++ mobile/lib/pages/signup.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:quizapp/componant/customTextField.dart';
 import 'package:quizapp/theme/theme.dart';
 
 class Signup extends StatelessWidget {
@@ -9,21 +10,81 @@ class Signup extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(bgcolor),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          Center(
-            child: Text(
-              'Quiz App',
-              style: TextStyle(
-                fontSize: 30,
-                fontFamily: 'pacifico',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            Center(
+              child: Text(
+                'Quiz App',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontFamily: 'pacifico',
+                ),
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Sign up',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text('Create Your Account'),
+            SizedBox(
+              height: 30,
+            ),
+            Customtextfield(
+                IsPassword: false, image: Icons.person, text: 'UserName'),
+            Customtextfield(
+                IsPassword: true, image: Icons.password, text: 'Password'),
+            Customtextfield(
+                IsPassword: true,
+                image: Icons.password,
+                text: 'Confirm Password'),
+            SizedBox(
+              height: 25,
+            ),
+            SizedBox(
+              width: 350,
+              height: 60,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(Buttoncolor),
+                ),
+                onPressed: () {},
+                child: Text(
+                  'Sign up',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Already Have an Account?   ',
+                  style: TextStyle(fontSize: 15),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Login',
+                    style: TextStyle(fontSize: 18, color: Color(Buttoncolor)),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
