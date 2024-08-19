308aa4ef62b0b84378bb064c0e531060e6e1ebd3
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-08-19 15:57:15 +0300

    update the sign_up and login pages to make them responsive and add navigator to start_quiz and create_quiz

--- mobile/lib/pages/create_quiz.dart
+++ mobile/lib/pages/create_quiz.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
-
+import 'package:quizapp/pages/home.dart';
 import '../theme/theme.dart';
 
-class CreateQuiz extends StatelessWidget{
+class CreateQuiz extends StatelessWidget {
   const CreateQuiz({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            );
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
         backgroundColor: Color(textcolor),
-        title: Text('Create new quiz' , style: TextStyle(color: Colors.white ,
+        title: Text(
+          'Create new quiz',
+          style: TextStyle(
+            color: Colors.white,
             fontSize: 25,
           ),
         ),
